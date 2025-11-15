e381e2ce8832caf119a4f539330bd159b2efe2fe
Shivshankar Kannoujiya <kannoujiyaji143@gmail.com>
2025-11-15 16:24:50 +0530

    Add implementation of array reversal and print functions

--- Class-01/08_reverseArray.cpp
+++ Class-01/08_reverseArray.cpp
@@ -1,7 +1,30 @@
 #include <iostream>
 using namespace std;
 
+void print_array(int arr[], int size){
+    for (int i = 0; i < size; i++){
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+void reverse_array(int arr[], int size){
+    int left = 0;
+    int right = size - 1;
+
+    while(left <= right){
+        swap(arr[left], arr[right]);
+        left++;
+        right--;
+    }
+}
 
 int main(){
-    
+
+    int arr[7] = {10, 20, 30, 40, 50, 60,70};
+    int size = 7;
+
+    print_array(arr, size);
+    reverse_array(arr, size);
+    print_array(arr, size);
 }
