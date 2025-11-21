15f12af225129d397ca82e948ba2a0241118cab5
Shivshankar Kannoujiya <kannoujiyaji143@gmail.com>
2025-11-21 14:48:02 +0530

    Add implementation of first_occurence

---
+++ 02_Searching_Sorting/Class-01/03_first_occurence.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int firstOccurence(vector<int>& v, int target){
+    int low = 0;
+    int high = v.size();
+    int ans = -1;
+
+    int mid = low + (high - low) / 2;
+
+    while(low <= high){
+        if(v[mid] == target){
+            ans = mid;
+            high = mid - 1;
+        }else if(target > v[mid]){
+            low = mid + 1;
+        }else if(target < v[mid]){
+            high = mid - 1;
+        }
+        mid = low + (high - low) / 2;
+    }
+
+    return ans;
+}
+
+int main(){
+    vector<int> v{30, 30, 30, 30, 30, 50, 70};
+
+    int target;
+    cout << "ENTER TARGET_VALUE:";
+    cin >> target;
+
+    int ans_index = firstOccurence(v, target);
+    cout << "FIRST OCCURENCE OF TARGET VALUE FOUND AT_INDEX: " << ans_index << endl;
+}
