8c299f85cedb2ac6262d50e0ec8a87bbe552f47c
Shivshankar Kannoujiya <kannoujiyaji143@gmail.com>
2025-11-24 01:22:09 +0530

    Handle -ve number case

--- 02_Searching_Sorting/Class-03/01_divide_BS.cpp
+++ 02_Searching_Sorting/Class-03/01_divide_BS.cpp
@@ -25,6 +25,40 @@ int getQuotient(int dividend, int divisor){
     return ans;
 }
 
+int getQuotientWithNegative(int dividend, int divisor){
+
+    if (divisor == 0)
+        return -1;
+    if (dividend == 0)
+        return 0;
+
+    bool is_negative = (dividend < 0) ^ (divisor < 0);
+
+    long long positive_dividend = llabs(dividend);
+    long long positive_divisor = llabs(divisor);
+
+
+    long long low = 0;
+    long long high = positive_dividend;
+    long long ans = -1;
+
+ 
+    while(low <= high){
+        int mid = low + (high - low) / 2;
+        if(mid * positive_divisor <= positive_dividend){
+            ans = mid;
+            low = mid + 1;
+        }else{
+            high = mid - 1;
+        }
+    }
+
+    if(is_negative)
+        return ans = -ans;
+
+    return ans;
+}
+
 int main(){
 
     int divisor;
@@ -35,6 +69,12 @@ int main(){
     cout << "ENTER DIVISOR: ";
     cin >> divisor;
 
-    int quotient = getQuotient(dividend, divisor);
+    // int quotient = getQuotient(abs(dividend), abs(divisor));
+
+    // if((dividend > 0 && divisor < 0 ) || (dividend < 0 && divisor > 0 )){
+    //     quotient = 0 - quotient;
+    // }
+
+    int quotient = getQuotientWithNegative(dividend, divisor);
     cout << "QUOTIENT IS: " << quotient << endl;
 }
