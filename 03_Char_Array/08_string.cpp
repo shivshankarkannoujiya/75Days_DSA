9b77161370f2a9c4aa6e2d96a8079b6a72aabf1f
Shivshankar Kannoujiya <kannoujiyaji143@gmail.com>
2025-11-27 17:19:36 +0530

    Add implementation of basic string input and output in character arrays

---
+++ 03_Char_Array/08_string.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+
+int main(){
+
+    string name;
+
+    // input
+    cout << "ENTER NAME: ";
+    // cin >> name;
+    getline(cin, name);
+
+    // print
+    // cout << "PRINTIN NAME: " << name << endl;
+    // cout << "PRINTIN FIRST CHAR OF NAME: " << name[0] << endl;
+
+    // int index = 0;
+    // while (name[index] != '\0'){
+    //     cout << index << ": " << name[index]<< endl;
+    //     index++;
+    // }
+    
+}
