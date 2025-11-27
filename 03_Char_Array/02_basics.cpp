1599403b076197172f4db7b5994dc1734a9a2f6d
Shivshankar Kannoujiya <kannoujiyaji143@gmail.com>
2025-11-27 14:38:51 +0530

    Add basic char array input and output functionality

---
+++ 03_Char_Array/02_basics.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+
+int main(){
+
+    char ch[5];
+
+    cout << "ENTER INPUT: ";
+    cin.getline(ch, 20);
+
+    cout << "PRINTING: " << ch << endl;
+}
