23a05564d72789826b5a2f12f77334937310a2ea
Shivshankar Kannoujiya <kannoujiyaji143@gmail.com>
2025-11-29 03:24:36 +0530

    Add implementation of duplicate removal in character arrays

---
+++ 03_Char_Array/Class-02/01_remove_duplicates.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+
+string removeDuplicate(string s){
+    string ans = "";
+
+    int index = 0;
+    while(index < s.length()){
+        if(ans.length() > 0 && ans[ans.length() - 1] == s[index]){
+            ans.pop_back();
+        }else{
+            ans.push_back(s[index]);
+        }
+        index++;
+    }
+    return ans;
+}
+
+int main(){
+    string str = "abbacy";
+    string ans = removeDuplicate(str);
+    cout << "ANS: " << ans << endl;
+}
